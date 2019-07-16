a19acba0b2fbc7f22235b638d79cf3fc85b01e21
hocineimail <hocineismail75@gmail.com>
2019-07-16 21:52:37 +0200

    Added pagination in page search

--- routes/Visiteur/Routes.js
+++ routes/Visiteur/Routes.js
@@ -4,7 +4,7 @@ const async =  require("async");
 // const multer = require('multer');
 const bodyParser = require("body-parser");
 const path = require("path");
-
+const Client = require("../../models/client")
 
 routes.get("/", (req, res) => {
     
@@ -14,8 +14,32 @@ routes.get("/", (req, res) => {
 
 routes.get("/search", (req, res) => {
     
-    res.render("Home/Search")
+    res.render("Home/Search", {
+        
+        current: 2,
+        pages: 10
+    })
     
+})
+
+routes.get('/search/:page', function(req, res, next) {
+    var perPage = 8
+    var page = req.params.page || 1
+
+    Client
+        .find({})
+        .skip((perPage * page) - perPage)
+        .limit(perPage)
+        .exec(function(err, clients) {
+            Client.count().exec(function(err, count) {
+                if (err) return next(err)
+                res.render('home', {
+                    clients: clients,
+                    current: page,
+                    pages: Math.ceil(count / perPage)
+                })
+            })
+        })
 })
 
 routes.get("/404", (req, res) => {
@@ -23,5 +47,7 @@ routes.get("/404", (req, res) => {
     res.render("Home/PageNotFound")
 
 })
+
+
          
 module.exports = routes;
