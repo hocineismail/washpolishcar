fc64ea17872a96b4e3e6251e6e2e35b8d23642ec
hocineimail <hocineismail75@gmail.com>
2019-07-26 00:32:47 +0200

    Fix bug  maps search

--- routes/Visiteur/Routes.js
+++ routes/Visiteur/Routes.js
@@ -38,56 +38,33 @@ routes.get("/", async (req, res) => {
     
 })
 
-routes.get("/search", (req, res) => {
-  const locations=[{ 
-      PositionLatitude: -33.8688,
-      PositionLongitude: 151.2195,
-      id:1,
-     },
-     { 
-     PositionLatitude: 28.033886,
-     PositionLongitude: 1.659626,
-     id:2,
-     } ]
-     res.render("Home/Search", {
-
-      current: 2,
-      pages: 10,
-      locations: locations
-  })
-
-})
-
-routes.get("/data", (req, res) => {
-   Location.find({}, (err, locations) => {
-     res.render("data", {locations: locations})
-   })
-})
-
-
-routes.get('/search/:page', function(req, res, next) {
-     
-    var perPage = 8
-    var page = req.params.page || 1
-
-    Client
-        .find({})
-        .populate('location')
-        .populate('start')
-        .skip((perPage * page) - perPage)
-        .limit(perPage)
-        .exec(function(err, clients) {
-            Client.countDocuments().exec(function(err, count) {
-                console.log(Math.ceil(count / perPage))
-                if (err) return next(err)
-                res.render("Home/Search", {
-                    clients: clients,
-                    current: page,
-                    pages: Math.ceil(count / perPage)
-                })
-            })
-        })
-})
+routes.get("/search",async (req, res) => {
+  return res.redirect("/search/1")
+ })
+ 
+ routes.get('/search/:page', function(req, res, next) {
+      
+     var perPage = 8
+     var page = req.params.page || 1
+ 
+     Client
+         .find({})
+         .populate('location')
+         .populate('start')
+         .skip((perPage * page) - perPage)
+         .limit(perPage)
+         .exec(function(err, clients) {
+             Client.countDocuments().exec(function(err, count) {
+                 console.log(Math.ceil(count / perPage))
+                 if (err) return next(err)
+                 res.render("Home/Search", {
+                     clients: clients,
+                     current: page,
+                     pages: Math.ceil(count / perPage)
+                 })
+             })
+         })
+ })
 
 routes.get("/404", (req, res) => {
   
